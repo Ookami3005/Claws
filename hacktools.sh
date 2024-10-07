2ac9c802dff0c5a87b818c1b1a7470955b9a0319
ookami <romerocruzfernando@gmail.com>
2024-10-07 12:01:43 -0600

    Beta: HackTools organizados

--- hacktools.sh
+++ hacktools.sh
@@ -2,18 +2,22 @@
 # Script que automatiza la instalación de las herramientas
 # que más utilizo en el Hacking Fight Club
 
+# Indicamos que se espera una instrucción
 if [[ $# -ne 1 ]] ; then
-    echo "No se recibio una instrucción"
+    echo "Error: No se recibio una instrucción"
     exit 1
 fi
 
+# Revisamos la instrucción dada
 if [[ $1 != "install" ]] && [[ $1 != "remove" ]] ; then
-    echo "Argumento incorrecto"
+    echo "Error: Argumento incorrecto"
     exit 1
 fi
 
+# Definimos a Nala como herramienta predeterminada para instalar los paquetes
 pac_man=nala
 
+# De no tener instalado Nala redefinimos la herramienta como APT
 if ! command -v nala > /dev/null ; then
     pac_man=apt
 fi
@@ -21,7 +25,55 @@ fi
 # Herramientas principales en los repositorios oficiales
 tools=(nmap ffuf hydra gobuster sqlmap)
 
+# Dependencias de John The Reaper
 john_deps=(git build-essential libssl-dev zlib1g-dev yasm pkg-config libgmp-dev libpcap-dev libbz2-dev)
+
+# Función que compila a John desde el repositorio oficial
+build_john () {
+
+    # Pedimos un argumento
+    if [[ $# -ne 1 ]] ; then
+        exit 1
+    fi
+
+    # Borrar (si existe) la versión de John del repositorio
+    command -v john > /dev/null && sudo $pac_man remove -y john 2> /dev/null
+
+    # Instalar las dependencias
+    sudo $pac_man install -y $john_deps
+
+    # Clonado del repo oficial
+    echo -e "\nClonando John del repositorio oficial...\n"
+    temp_dir=$(mktemp -d)
+    git clone https://github.com/openwall/john -b bleeding-jumbo "$1/john"
+
+    # Iniciando la compilación
+    echo -e "\nCompilando John...\n"
+    cd "$1/john/src" && ./configure && make -s clean && make -sj4
+
+    # Configuración de los ejecutables
+    echo -e "\nExtrayendo scripts...\n"
+
+    # Generamos un enlace simbólico por temas de compatibilidad con los scripts
+    sudo ln -s /usr/bin/python3 /sbin/python
+
+    # Definimos la carpeta donde almacenaremos los ejecutables
+    mkdir -p "$HOME/bin"
+    mkdir -p "$HOME/bin/2john"
+
+    # Comenzamos a transferir los ejecutables
+    cd "$1/john/run"
+    mv john "${HOME}/bin/2john/."
+    for item in *(2john|tojohn)* ; do
+        dest=$(cut -d "." -f 1 <<< $item)
+        mv $item "${HOME}/bin/2john/$dest"
+    done
+    cd
+
+    # Añadimos la carpeta al PATH
+    echo -e "\nAñadiendo al PATH...\n"
+    echo "export PATH=\"${HOME}/bin/2john:$PATH\"" >> $HOME/.zshrc
+}
 
 echo -e "Instalando herramientas del repositorio...\n"
 sudo $pac_man $1 -y $tools
@@ -32,34 +84,7 @@ if [[ $1 = "install" ]]; then
     read elec
 
     if [[ $elec = s ]]; then
-
-        # Borrar (si existe) la versión de John del repositorio
-        command -v john > /dev/null && sudo $pac_man remove -y john 2> /dev/null
-
-        # Dependencias
-        sudo $pac_man install -y $john_deps
-
-        # Clonado del repo oficial
-        echo -e "Clonando John del repositorio oficial...\n"
-        temp_dir=$(mktemp -d)
-        git clone https://github.com/openwall/john -b bleeding-jumbo "$temp_dir/john"
-        echo -e "Compilando John...\n"
-        cd "$temp_dir/john/src" && ./configure && make -s clean && make -sj4
-
-        echo -e "\nExtrayendo scripts...\n"
-        sudo ln -s /usr/bin/python3 /sbin/python
-        cd "$temp_dir/john/run"
-        mkdir -p "$HOME/bin"
-        mkdir -p "$HOME/bin/2john"
-        mv john "${HOME}/bin/2john/."
-        for item in *(2john|tojohn)* ; do
-            dest=$(cut -d "." -f 1 <<< $item)
-            mv $item "${HOME}/bin/2john/$dest"
-        done
-
-        echo -e "\nAñadiendo al PATH...\n"
-        echo "export PATH=\"${HOME}/bin/2john:$PATH\"" >> $HOME/.zshrc
-        cd
+        build_john $(mktemp -d)
     fi
 fi
 
