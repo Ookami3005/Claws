379f7602d229622d2e8762eb9cdf81e8bc5472df
ookami <romerocruzfernando@gmail.com>
2025-01-29 01:27:11 -0600

    Borre john.sh

--- john.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-mkdir -p ${HOME}/.local/src
-git clone https://github.com/openwall/john.git ${HOME}/.local/src/john
-cd ${HOME}/.local/src/john/src/ || exit 1
-./configure && make -s clean && sudo make -sj4
-sudo ln -sv ${HOME}/.local/src/john/run/ssh2john.py /usr/local/bin/ssh2john
